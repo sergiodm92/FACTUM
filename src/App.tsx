bf10c25c6d600637c5d0b05718ec168de3789f4e
sergiodm92 <crgiodm@gmail.com>
2024-11-06 13:05:40 -0300

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { ThemeProvider } from './contexts/ThemeContext'
-import { LanguageProvider } from './contexts/LanguageContext'
-import MainLayout from './layouts/MainLayout'
-
-import HomePage from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/LanguageContext';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/Home';
+import DevelopmentPage from './pages/Development';
+import ConsultingPage from './pages/Consulting';
+import NotFoundPage from './pages/NotFound';
 
 function App() {
   return (
     <ThemeProvider>
       <LanguageProvider>
-        <MainLayout>
-          <HomePage/>
-        </MainLayout>
+        <Router>
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/development" element={<DevelopmentPage />} />
+              <Route path="/consulting" element={<ConsultingPage />} />
+              <Route path="*" element={<NotFoundPage />} /> {/* Catch-all 404 Route */}
+            </Routes>
+          </MainLayout>
+        </Router>
       </LanguageProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
